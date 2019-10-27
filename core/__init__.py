a3170118966f051cee657deda1dd28242ed3e7c7
zeffii <tetha.z@gmail.com>
2019-10-27 16:15:15 +0100

    add node_cat file

--- core/__init__.py
+++ core/__init__.py
@@ -1,10 +1,17 @@
-from flux.core import sockets, node_tree
+# This file is part of project Sverchok. It's copyrighted by the contributors
+# recorded in the version control history of the file, available from
+# its original location https://github.com/zeffii/flux/commit/master
+#  
+# SPDX-License-Identifier: GPL3
+# License-Filename: LICENSE
 
+from flux.core import sockets, node_tree, node_categories
 
+modules = [sockets, node_tree, node_categories]
 def register():
-    for module in [sockets, node_tree]:
+    for module in modules:
         module.register()
 
 def unregister():
-    for module in [sockets, node_tree]:
+    for module in reversed(modules):
         module.unregister()
--- core/node_tree.py
+++ core/node_tree.py
@@ -61,56 +61,5 @@ class FluxTestNode(Node, FluxCustomTreeNode):
         return "I am a custom node"
 
 
-### Node Categories ###
-# Node categories are a python system for automatically
-# extending the Add menu, toolbar panels and search operator.
-# For more examples see release/scripts/startup/nodeitems_builtins.py
-
-import nodeitems_utils
-from nodeitems_utils import NodeCategory, NodeItem
-
-# our own base class with an appropriate poll function,
-# so the categories only show up in our own tree type
-
-
-class FluxNodeCategory(NodeCategory):
-    @classmethod
-    def poll(cls, context):
-        return context.space_data.tree_type == 'FluxCustomTree'
-
-
-# all categories in a list
-node_categories = [
-    # identifier, label, items list
-    FluxNodeCategory('SOMENODES', "Some Nodes", items=[
-        NodeItem("FluxTestNode"),
-    ]),
-    FluxNodeCategory('OTHERNODES', "Other Nodes", items=[
-        NodeItem("FluxTestNode", label="Node A", settings={
-            "my_string_prop": repr("Lorem ipsum dolor sit amet"),
-            "my_float_prop": repr(1.0),
-        }),
-        NodeItem("FluxTestNode", label="Node B", settings={
-            "my_string_prop": repr("consectetur adipisicing elit"),
-            "my_float_prop": repr(2.0),
-        }),
-    ]),
-]
-
 classes = (FluxCustomTree, FluxTestNode)
-
-
-def register():
-    from bpy.utils import register_class
-
-    for cls in classes:
-        register_class(cls)
-    nodeitems_utils.register_node_categories('CUSTOM_NODES', node_categories)
-
-
-def unregister():
-    from bpy.utils import unregister_class
-
-    nodeitems_utils.unregister_node_categories('CUSTOM_NODES')
-    for cls in reversed(classes):
-        unregister_class(cls)
+register, unregister = bpy.utils.register_classes_factory(classes)

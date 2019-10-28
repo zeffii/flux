c770c8d3557f27aeb0fd39ece90b08106c757a95
zeffii <tetha.z@gmail.com>
2019-10-28 14:12:31 +0100

    tests automated registration

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,9 +1,24 @@
+import inspect
 import flux
 
 from flux.nodes.tests import test_node
 from flux.nodes.other_tests import test_node_2, test_node_3
 
-modules = test_node, test_node_2, test_node_3
+
+def make_modules():
+    modules = []
+    append_modules = modules.append
+
+    def append_modules_func(node_modules):
+        for item in node_modules:
+            if hasattr(item[1], 'register'):
+                append_modules(item[1])
+
+    append_modules_func(inspect.getmembers(tests, inspect.ismodule))
+    append_modules_func(inspect.getmembers(other_tests, inspect.ismodule))
+    return modules
+
+modules = make_modules()
 
 
 def register():
